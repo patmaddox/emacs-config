3f5636a98bd53e89bc1459f39d57b7d6c7508ba7
Pat Maddox <pat@ratiopbc.com>
2022-05-02 11:12:54 -0700

    org keywords

--- early-init.el
+++ early-init.el
@@ -1,2 +1,8 @@
 ;; straight.el says to do this
 (setq package-enable-at-startup nil)
+
+;; org / agenda config
+(setq org-agenda-files
+      '("~/org"))
+(setq org-todo-keywords
+      '((sequence "TODO" "NEXT" "WAITING" "|" "DONE")))
